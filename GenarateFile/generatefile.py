efea0bdaddab66e01382ea721b448756a32e64d1
davidlm12 <lizarraga.mendoza.david@gmail.com>
2021-06-22 20:15:11 -0500

    Actualizacion de rama

---
+++ GenarateFile/generatefile.py
@@ -0,0 +1,8 @@
+import numpy as np
+import os
+intRango = np.arange(1, 101) #Elegir rango de datos
+numeros = np.random.choice(intRango, size = (100), replace = False) # Elegir el numero de datos a generar
+file = open("drive//MyDrive//Colab//X100.txt", "w") # Seleccionar nombre del archivo
+for x in numeros:
+  file.write(str(x))
+file.close()
